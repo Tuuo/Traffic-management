901e64865166de92598d68c55e3c8f3a086dab78
Tuuo <lsfxwxq@163.com>
2023-11-13 08:24:36 +0800

    最长的一个

--- user-service/src/main/java/com/isoft/UserServiceApplication.java
+++ user-service/src/main/java/com/isoft/UserServiceApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 //@ComponentScan(basePackages = {"com.isoft.mapper"})
 @MapperScan("com.isoft.dao")
 //@EnableDiscoveryClient
+@CrossOrigin
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
